bcfb663a7de8972d3492bd7e84131a2080918aff
Vadad Rzali <vedad.rzali@gmail.com>
2022-09-14 20:50:26 -0400

    Install button bug

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,15 +3,20 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    butInstall.style.visibility = 'visible';
-  
-    butInstall.addEventListener('click', () => {
-      event.prompt();
-      butInstall.setAttribute('disabled', true);
-      butInstall.textContent = 'Installed!';
-    });
-  });
+    window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
+});
   
   window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
