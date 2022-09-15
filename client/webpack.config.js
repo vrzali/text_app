69df79fb60004c6f911d8dd28d0d704c2eec2e94
Vadad Rzali <vedad.rzali@gmail.com>
2022-09-14 20:39:40 -0400

    Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'production',
+    mode: 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
